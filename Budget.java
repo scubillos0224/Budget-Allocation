bf128560da2c5304ae763b6ddaf930125903fee0
Sebastian Cubillos <71351950+scubillos0224@users.noreply.github.com>
2023-02-24 01:51:24 -0500

    Add files via upload

---
+++ Budget.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Budget {
+    public static void main(String[] args) {
+        System.out.println("How much are you able to spend?");
+        Scanner scnr = new Scanner(System.in);
+        double total = scan.nextDouble();
+        double sum = 0;
+        int i = 0;
+        ArrayList<Double> proportion = new ArrayList<Double>();
+    }
+}
